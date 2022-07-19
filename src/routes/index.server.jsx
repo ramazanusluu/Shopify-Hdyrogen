d4e77822b6263ba7a75b5b02cc38a289e79164da
Ramazan USLU <ruslu1720@gmail.com>
2022-07-19 14:15:50 +0300

    First Commit

---
+++ src/routes/index.server.jsx
@@ -0,0 +1,10 @@
+import FeaturedCollections from "../components/FeaturedCollections.server";
+import { Layout } from "../components/Layout.server";
+
+export default function Home() {
+  return (
+    <Layout>
+      <FeaturedCollections />
+    </Layout>
+  );
+}
