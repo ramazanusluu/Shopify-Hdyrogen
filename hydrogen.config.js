682938ef60679f91064c4f029ad2e350a4586991
Ramazan USLU <ruslu1720@gmail.com>
2022-08-02 21:22:49 +0300

    Build a product page

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -5,5 +5,6 @@ export default defineConfig({
     storeDomain: "hydrogen-preview.myshopify.com",
     storefrontToken: "3b580e70970c4528da70c98e097c2fa0",
     storefrontApiVersion: "2022-07",
+    strictMode: false,
   },
 });
